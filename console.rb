2ed5d64751647d9e704f9bdf71718c1cd707d659
Callum Mackenzie <callum1mackenzie@gmail.com>
2018-04-11 21:17:58 +0100

    album and artist class save via looping array in console.rb

--- console.rb
+++ console.rb
@@ -4,46 +4,54 @@ require('pry-byebug')
 Album.delete_all()
 Artist.delete_all()
 
-artist1 = Artist.new({
-  "name" => "the beta band"
-  })
-  artist1.save()
-artist2 = Artist.new({
-  "name" => "americian football"
-  })
-  artist2.save()
-artist3 = Artist.new({
-  "name" => "anchorsong"
-  })
-  artist3.save()
-artist4 = Artist.new({
-  "name" => "artic monkeys"
-  })
-  artist4.save()
-  album1 = Album.new({
-    "name" => "yellow",
-    "genre" => "blue",
-    "artist_id" => artist1.id
-    })
-    album1.save()
-  album2 = Album.new({
-    "name" => "red",
-    "genre" => "dub step",
-    "artist_id" => artist2.id
-    })
-    album2.save()
-    album3 = Album.new({
-      "name" => "green",
-      "genre" => "indie",
-      "artist_id" => artist3.id
-      })
-      album3.save()
-    album4 = Album.new({
-      "name" => "black",
-      "genre" => "indie",
-      "artist_id" => artist3.id
-    })
-    album4.save()
-binding.pry
+artist_array = [
+  artist1 = Artist.new(
+    {
+      "name" => "the beta band"
+      }),
+      artist2 = Artist.new(
+        {
+          "name" => "americian football"
+          }),
+          artist3 = Artist.new(
+            {
+              "name" => "anchorsong"
+              }),
+              artist4 = Artist.new(
+                {
+                  "name" => "artic monkeys"
+                  }),
+                  artist5 = Artist.new(
+                    {
+                      "name" => "a perfect circle"
+                      })]
 
-nil
+                      artist_array.each {|artist| artist.save()}
+                      binding.pry
+
+                      album_array = [
+                        album1 = Album.new({
+                          "name" => "yellow",
+                          "genre" => "blue",
+                          "artist_id" => artist1.id
+                          }),
+                          album2 = Album.new({
+                            "name" => "red",
+                            "genre" => "dub step",
+                            "artist_id" => artist2.id
+                            }),
+                            album3 = Album.new({
+                              "name" => "green",
+                              "genre" => "indie",
+                              "artist_id" => artist3.id
+                              }),
+                              album4 = Album.new({
+                                "name" => "black",
+                                "genre" => "indie",
+                                "artist_id" => artist3.id
+                                })]
+                                album_array.each {|album| album.save()}
+
+                                binding.pry
+
+                                nil
